2c94cf2428ce3c3cfeae44ef011e2ccca918bace
mben-devops <matthewbenson2000@gmail.com>
2025-10-25 17:14:04 -0700

    New header placeholder

---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <header className="flex justify-between bg-gray-500 border-black border-2 p-2">
+            <text>Left</text>
+            <text className="">No head?</text>
+            <text>Right</text>
+        </header>
+    )
+}
